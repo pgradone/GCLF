315b9c6c45d8eac77b7693c3fb507309acf6dd93
Potito Gradone <pgradone@gmail.com>
2020-07-15 20:16:45 +0200

    fixed bugs movie-detail

--- Project/movie-details.php
+++ Project/movie-details.php
@@ -8,49 +8,48 @@
 </head>
 
 <body>
-    <?php require_once 'nav.php';?>
+    <?php require_once 'nav.php'; ?>
     <?php
-// Make sur I get an ID
-if (isset($_GET['id'])) {
+    // Make sur I get an ID
+    if (isset($_GET['id'])) {
 
-    // Make sure it is a number I get
-    $movieID = (int) $_GET['id'];
+        // Make sure it is a number I get
+        $movieID = (int) $_GET['id'];
 
-    // Make sure it's an number AND a valid ID
-    if ($movieID > 0) {
-        $db_name = 'gclf';
-        $db_handle = mysqli_connect('localhost', 'root', '', $db_name);
-        $db_found = mysqli_select_db($db_handle, $db_name);
+        // Make sure it's an number AND a valid ID
+        if ($movieID > 0) {
+            $db_name = 'gclf';
+            $db_handle = mysqli_connect('localhost', 'root', '', $db_name);
+            $db_found = mysqli_select_db($db_handle, $db_name);
 
-        if ($db_found) {
+            if ($db_found) {
 
-            $sql_query = 'SELECT m.*, c.gender 
-            FROM movies m 
-            INNER JOIN categories c ON c.category_id = m.category_id
-            WHERE m.id = ' . $movieID;
+                $sql_query = 'SELECT m.*, c.gender FROM movies m 
+                    LEFT JOIN categories c ON c.category_id = m.category_id 
+                    WHERE m.movie_id = ' . $movieID;
 
-            $result_query = mysqli_query($db_handle, $sql_query);
-            $movie = mysqli_fetch_assoc($result_query);
+                $result_query = mysqli_query($db_handle, $sql_query);
+                $movie = mysqli_fetch_assoc($result_query);
 
-            echo '<img href="' . $movie['poster'] . '" alt="' . $movie['title'] . '">';
-            echo '<p><strong>Title : </strong>' . $movie['title'] . '</p>';
-            echo '<p><strong>Year of release : </strong>' . $movie['year_released'] . '</p>';
-
-            echo '<br>';
-            echo '<p><strong>Caregory : </strong>' . $movie['gender'] . '</p>';
+                echo '<div style="display:flex"></div><img src="' . $movie['poster'] . '" alt="' . $movie['title'] . '" style="height:200px" ">';
+                echo '<p><strong>Title : </strong>' . $movie['title'] . '</p>';
+                echo '<p><strong>Category : </strong>' . $movie['gender'] . '</p></div>';
+                echo '<p><strong>Synopsis : </strong>' . $movie['synopsis'] . '</p>';
+                echo '<p><strong>Released : </strong>' . $movie['year_released'] . '</p>';
+                echo '<p><strong>Caregory : </strong>' . $movie['gender'] . '</p>';
+            } else {
+                echo 'DB not found (' . $db_name . ')';
+            }
         } else {
-            echo 'DB not found (' . $db_name . ')';
+            echo 'Something\'s wrong... movie_id not OK!';
+            echo '<a href="./">Go Back</a>';
         }
     } else {
-        echo 'Something\'s wrong1... movie_id not OK!';
+        echo 'Something\'s wrong... no movie_id provided!';
         echo '<a href="./">Go Back</a>';
     }
-} else {
-    echo 'Something\'s wrong... no movie_id provided!';
-    echo '<a href="./">Go Back</a>';
-}
 
-?>
+    ?>
 
 
 </body>
