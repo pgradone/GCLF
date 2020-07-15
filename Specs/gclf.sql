cd46f0967ece087f52ff8b312eb5148c8d01570b
Potito Gradone <pgradone@gmail.com>
2020-07-15 20:18:30 +0200

    new script with movies categories and synopsis...

--- Specs/gclf.sql
+++ Specs/gclf.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 15, 2020 at 12:46 PM
--- Server version: 5.7.30-log
+-- Generation Time: Jul 15, 2020 at 06:17 PM
+-- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -41,12 +41,12 @@ CREATE TABLE IF NOT EXISTS `act` (
 --
 
 INSERT INTO `act` (`movie_id`, `actor_id`) VALUES
-(2, 3),
-(3, 4),
-(3, 5),
 (4, 2),
 (5, 2),
-(10, 2);
+(10, 2),
+(2, 3),
+(3, 4),
+(3, 5);
 
 -- --------------------------------------------------------
 
@@ -125,7 +125,7 @@ CREATE TABLE IF NOT EXISTS `movies` (
   `movie_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id of the movie',
   `title` varchar(50) NOT NULL,
   `year_released` int(11) DEFAULT NULL COMMENT 'year of release',
-  `synopsis` varchar(500) DEFAULT NULL COMMENT 'synopsis of the film',
+  `synopsis` varchar(2000) DEFAULT NULL COMMENT 'synopsis of the film',
   `poster` varchar(1000) DEFAULT NULL,
   `category_id` int(11) DEFAULT NULL COMMENT 'category id of movie',
   PRIMARY KEY (`movie_id`),
@@ -138,16 +138,16 @@ CREATE TABLE IF NOT EXISTS `movies` (
 --
 
 INSERT INTO `movies` (`movie_id`, `title`, `year_released`, `synopsis`, `poster`, `category_id`) VALUES
-(1, 'Jurassic Park', 1993, '2', 'https://images-na.ssl-images-amazon.com/images/I/51dZZ4pl-kL._AC_.jpg', NULL),
-(2, 'Scarface', 1983, '4', 'https://thewallpaper.co//wp-content/uploads/2019/10//gun-film-movie-drama-dark-weapon-crime-poster-movie-wallpaper-scarface-cinema-images-hd-wallpaper-jpg.jpg', NULL),
-(3, 'Salò', 1975, '3', 'https://cdn.shopify.com/s/files/1/1057/4964/products/salo-vintage-movie-poster-original-italian-2-foglio-39x55-6073_1491x.jpg?v=1534413233', NULL),
-(4, 'Star Wars I', 1978, '5', 'https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_.jpg', NULL),
-(5, 'Indiana Jones', 1981, '2', 'https://m.media-amazon.com/images/M/MV5BMTIxNDUxNzcyMl5BMl5BanBnXkFtZTcwNTgwOTI3MQ@@._V1_UY1200_CR90,0,630,1200_AL_.jpg', NULL),
-(6, 'Teorema', 1968, '3', 'https://m.media-amazon.com/images/M/MV5BZjNlMzM5MGYtMmRiYS00NDlhLThjMTgtYmUwYzVhYzVlZjgyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNzc5MjA3OA@@._V1_UY1200_CR79,0,630,1200_AL_.jpg', NULL),
-(7, 'Oedipus Rex', 1967, '3', 'https://www.gstatic.com/tv/thumb/v22vodart/31308/p31308_v_v8_aa.jpg', NULL),
-(8, 'The Real Thing', 2002, '6', 'https://m.media-amazon.com/images/M/MV5BMTc4NjcxMjczMF5BMl5BanBnXkFtZTcwMjg0MDcyMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg', NULL),
-(9, 'Something Fishy', 1994, '1', 'https://m.media-amazon.com/images/M/MV5BMWI3ZjAwMjEtNmU1Zi00MDU4LTk5M2QtNWViMjRiMGVhYjg1XkEyXkFqcGdeQXVyNTY1MDY1NjY@._V1_SY1000_CR0,0,734,1000_AL_.jpg', NULL),
-(10, 'Raiders of the Lost Ark', 1981, '5', 'https://m.media-amazon.com/images/M/MV5BMjA0ODEzMTc1Nl5BMl5BanBnXkFtZTcwODM2MjAxNA@@._V1_UY1200_CR84,0,630,1200_AL_.jpg', NULL);
+(1, 'Jurassic Park', 1993, 'A pragmatic paleontologist visiting an almost complete theme park is tasked with protecting a couple of kids after a power failure causes the park\'s cloned dinosaurs to run loose.', 'https://images-na.ssl-images-amazon.com/images/I/51dZZ4pl-kL._AC_.jpg', 3),
+(2, 'Scarface', 1983, 'In 1980 Miami, a determined Cuban immigrant takes over a drug cartel and succumbs to greed.', 'https://thewallpaper.co//wp-content/uploads/2019/10//gun-film-movie-drama-dark-weapon-crime-poster-movie-wallpaper-scarface-cinema-images-hd-wallpaper-jpg.jpg', 4),
+(3, 'Salò', 1975, 'In World War II Italy, four fascist libertines round up nine adolescent boys and girls and subject them to one hundred and twenty days of physical, mental and sexual torture.', 'https://cdn.shopify.com/s/files/1/1057/4964/products/salo-vintage-movie-poster-original-italian-2-foglio-39x55-6073_1491x.jpg?v=1534413233', 5),
+(4, 'Star Wars I', 1978, 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire\'s world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.', 'https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_.jpg', 1),
+(5, 'Indiana Jones', 1981, 'In 1935, Indiana Jones arrives in India, still part of the British Empire, and is asked to find a mystical stone. He then stumbles upon a secret cult committing enslavement and human sacrifices in the catacombs of an ancient palace.', 'https://m.media-amazon.com/images/M/MV5BMTIxNDUxNzcyMl5BMl5BanBnXkFtZTcwNTgwOTI3MQ@@._V1_UY1200_CR90,0,630,1200_AL_.jpg', 3),
+(6, 'Teorema', 1968, 'A mysterious young man seduces each member of a bourgeois family. When he suddenly leaves, how will their lives change?', 'https://m.media-amazon.com/images/M/MV5BZjNlMzM5MGYtMmRiYS00NDlhLThjMTgtYmUwYzVhYzVlZjgyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNzc5MjA3OA@@._V1_UY1200_CR79,0,630,1200_AL_.jpg', 5),
+(7, 'Oedipus Rex', 1967, 'Rescued from abandonment and raised by the King and Queen, Oedipus is still haunted by a prophecy--he\'ll murder his father and marry his mother.', 'https://www.gstatic.com/tv/thumb/v22vodart/31308/p31308_v_v8_aa.jpg', 4),
+(8, 'The Real Thing', 2002, 'Rupert is a small time crook trying to do the right thing. But when his younger brother gets in trouble and needs a new liver, Rupert gets together his old gang and they plan a heist on New Years Eve.', 'https://m.media-amazon.com/images/M/MV5BMTc4NjcxMjczMF5BMl5BanBnXkFtZTcwMjg0MDcyMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg', 4),
+(9, 'Something Fishy', 1994, 'Maxime is the best detective of a French agency. She is always very busy driving many investigations at one time. She divorced 15 years before, when she got the job. During an investigation she finds her son, who had not seen since he was a baby, so she begins to have memories of her past life that she belived forgotten. Another investigation leads her to discover dirty bussiness of her ex-husband.', 'https://m.media-amazon.com/images/M/MV5BMWI3ZjAwMjEtNmU1Zi00MDU4LTk5M2QtNWViMjRiMGVhYjg1XkEyXkFqcGdeQXVyNTY1MDY1NjY@._V1_SY1000_CR0,0,734,1000_AL_.jpg', 6),
+(10, 'Raiders of the Lost Ark', 1981, 'Spring 1936. In the thick jungle of the South American continent, a renowned archeologist and expert on the occult is studying fragments of a map, when one of his exploration party pulls a gun. The archeologist pulls out a bullwhip and with such disarms the turncoat, sending him running - thus does Dr. Henry \"Indiana\" Jones stay alive. He and a guide enter a dank and oppressively vast cave that contains several traps created by the ancient race which hid inside a famous handheld statue; Indy barely escapes such traps but is cornered by native tribesmen served by Belloq, an old enemy who arrogantly makes off with the statue, while Indy must flee for his life and escape on a friend\'s seaplane. Back in the US two agents from US Army intelligence tell him of Nazi German activities in archeology, including a gigantic excavation site in Egypt - a site that an intercepted cable indicates to Indy is the location of the Ark of the Covenant, the powerful chest bearing the Ten Commandments, that the Nazis can use to obliterate any enemy. Indy must recruit a former girlfriend (the daughter of his old professor) and an old chum in Cairo to infiltrate the Nazi site and make off with the Ark, but along the way Indy gets involved in a series of fights, chases, and traps, before the Nazis learn the full power of the Ark.', 'https://m.media-amazon.com/images/M/MV5BMjA0ODEzMTc1Nl5BMl5BanBnXkFtZTcwODM2MjAxNA@@._V1_UY1200_CR84,0,630,1200_AL_.jpg', 3);
 
 -- --------------------------------------------------------
 
