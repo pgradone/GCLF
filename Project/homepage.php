a89f590e1f7dcbaa253d119dae3b28df2ebcd32c
GuiPeras <gui_udemy1@protonmail.com>
2020-07-15 16:16:54 +0200

    both styles are linked to the specific pages.

--- Project/homepage.php
+++ Project/homepage.php
@@ -8,7 +8,7 @@ require_once 'nav.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-home.css">
     <title>Home</title>
 </head>
 
