10f0033f49e403109aacfe6da8d16b691d7d329e
GuiPeras <gui_udemy1@protonmail.com>
2020-07-15 16:15:38 +0200

    i put the css file of the nav template on the styles folder and also added my own styles folder to the folder

--- Project/homepage.php
+++ Project/homepage.php
@@ -8,11 +8,63 @@ require_once 'nav.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Home</title>
 </head>
 
 <body>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus illo, tempora sit consequatur dolorem
+        dignissimos accusantium voluptatum corrupti obcaecati sapiente in laborum delectus animi minus natus, ad iste at
+        alias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nihil eum quidem quod qui possimus omnis
+        consequuntur officiis, deserunt repellendus optio animi magnam, assumenda rem, natus illum ea aut corporis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum consequuntur corrupti eveniet excepturi inventore
+        voluptatem, harum adipisci repellat incidunt nesciunt, dignissimos nulla! Temporibus facere inventore suscipit
+        excepturi veniam sapiente! Facere!
+    </p>
 
+    <form action="" method="post">
+        <input type="search" name="search">
+        <input type="submit" name="submit" value="search movie">
+    </form>
+
+    <div>
+        <a href="">adventure</a>
+        <a href="">comedy</a>
+        <a href="">drama</a>
+        <a href="">horror</a>
+        <a href="">Sci-fi</a>
+        <a href="">triller</a>
+    </div>
+
+    <section>
+        <article>
+            <div>
+                <h3>film title</h3>
+            </div>
+            <h3>
+                <a href="">title</a>
+            </h3>
+        </article>
+
+        <article>
+            <div>
+                <h3>film title2</h3>
+            </div>
+            <h3>
+                <a href="">title</a>
+            </h3>
+        </article>
+
+        <article>
+            <div>
+                <h3>film title3</h3>
+            </div>
+            <h3>
+                <a href="">title</a>
+            </h3>
+        </article>
+    </section>
 </body>
 
 </html>
