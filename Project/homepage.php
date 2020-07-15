144354c2043b7a4052a26fb7ec13d92ed8c521ef
GuiPeras <gui_udemy1@protonmail.com>
2020-07-15 16:40:58 +0200

    the design of the homepage is done pretty much

--- Project/homepage.php
+++ Project/homepage.php
@@ -13,58 +13,72 @@ require_once 'nav.php';
 </head>
 
 <body>
-    <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus illo, tempora sit consequatur dolorem
-        dignissimos accusantium voluptatum corrupti obcaecati sapiente in laborum delectus animi minus natus, ad iste at
-        alias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nihil eum quidem quod qui possimus omnis
-        consequuntur officiis, deserunt repellendus optio animi magnam, assumenda rem, natus illum ea aut corporis.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum consequuntur corrupti eveniet excepturi inventore
-        voluptatem, harum adipisci repellat incidunt nesciunt, dignissimos nulla! Temporibus facere inventore suscipit
-        excepturi veniam sapiente! Facere!
-    </p>
+    <main>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus illo, tempora sit consequatur dolorem
+            dignissimos accusantium voluptatum corrupti obcaecati sapiente in laborum delectus animi minus natus, ad
+            iste at
+            alias? Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nihil eum quidem quod qui possimus
+            omnis
+            consequuntur officiis, deserunt repellendus optio animi magnam, assumenda rem, natus illum ea aut corporis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum consequuntur corrupti eveniet excepturi
+            inventore
+            voluptatem, harum adipisci repellat incidunt nesciunt, dignissimos nulla! Temporibus facere inventore
+            suscipit
+            excepturi veniam sapiente! Facere!
+        </p>
 
-    <form action="" method="post">
-        <input type="search" name="search">
-        <input type="submit" name="submit" value="search movie">
-    </form>
+        <form action="" method="post">
+            <input type="search" name="search" id="search">
+            <input type="submit" name="submit" value="search movie">
+        </form>
 
-    <div>
-        <a href="">adventure</a>
-        <a href="">comedy</a>
-        <a href="">drama</a>
-        <a href="">horror</a>
-        <a href="">Sci-fi</a>
-        <a href="">triller</a>
-    </div>
+        <div class="categories">
+            <a href="">adventure</a>
+            <a href="">comedy</a>
+            <a href="">drama</a>
+            <a href="">horror</a>
+            <a href="">Sci-fi</a>
+            <a href="">triller</a>
+        </div>
 
-    <section>
-        <article>
-            <div>
-                <h3>film title</h3>
-            </div>
-            <h3>
-                <a href="">title</a>
-            </h3>
-        </article>
+        <section class="categories">
+            <article>
+                <div id="card">
+                    <h3>film title</h3>
+                </div>
+                <h3>
+                    <a href="">title</a>
+                </h3>
+            </article>
 
-        <article>
-            <div>
-                <h3>film title2</h3>
-            </div>
-            <h3>
-                <a href="">title</a>
-            </h3>
-        </article>
+            <article>
+                <div id="card">
+                    <h3>film title2</h3>
+                </div>
+                <h3>
+                    <a href="">title</a>
+                </h3>
+            </article>
 
-        <article>
-            <div>
-                <h3>film title3</h3>
-            </div>
-            <h3>
-                <a href="">title</a>
-            </h3>
-        </article>
-    </section>
+            <article>
+                <div id="card">
+                    <h3>film title3</h3>
+                </div>
+                <h3>
+                    <a href="">title</a>
+                </h3>
+            </article>
+            <article>
+                <div id="card">
+                    <h3>film title3</h3>
+                </div>
+                <h3>
+                    <a href="">title</a>
+                </h3>
+            </article>
+        </section>
+    </main>
 </body>
 
 </html>
