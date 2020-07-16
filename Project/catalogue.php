b97dce906f02c1b73e114adc4200f386dde51630
pintoedu <pintoedu@gmail.com>
2020-07-16 11:00:41 +0200

    added links to movie details and added sorting form

--- Project/catalogue.php
+++ Project/catalogue.php
@@ -10,38 +10,48 @@
 
 <body>
     <?php require_once 'nav.php'; ?>
-    <h3>Movies catalogue</h3>
+    <h2>Movies catalogue</h2>
     <hr>
     <?php
 
     $conn = mysqli_connect("localhost", "root", "", "gclf");
-    $query = 'SELECT * FROM movies LIMIT 3';
+    $query = 'SELECT * FROM movies ORDER BY year_released ASC';
 
     $results = mysqli_query($conn, $query);
     $movies = mysqli_fetch_all($results, MYSQLI_ASSOC);
-
     //var_dump($results);
     mysqli_close($conn);
     ?>
-    <h3>Select by date</h3>
 
-    <?php foreach ($movies as $movie) : ?>
-        <hr>
-        <img src="<?= $movie['poster'] ?>" alt="" height="300" width="200">
-        <p>
-            <strong>ID:</strong>
-            <?= $movie['movie_id'] ?>
-        </p>
-        <p>
-            <strong>Title:</strong>
-            <?= $movie['title'] ?>
-        </p>
-        <p>
-            <strong>Synopsis:</strong>
-            <?= $movie['synopsis'] ?>
-        </p>
-        <a href="movie.php?id=<?= $movie['ID'] ?>">Details</a>
-    <?php endforeach; ?>
+
+    <form name="sort" method="POST">Order by year:</label>
+        <select name="order">
+            <option value="choose">Choose</option>
+            <option value="asc">ASC</option>
+            <option value="desc">DESC</option>
+            <input type="submit" value="Go">
+        </select>
+
+        <?php
+
+
+        foreach ($movies as $movie) : ?>
+            <hr>
+            <img src="<?= $movie['poster'] ?>" alt="" height="300" width="200">
+            <p>
+                <strong>ID:</strong>
+                <?= $movie['movie_id'] ?>
+            </p>
+            <p>
+                <strong>Title:</strong>
+                <?= $movie['title'] ?>
+            </p>
+            <p>
+                <strong>Synopsis:</strong>
+                <?= $movie['synopsis'] ?>
+            </p>
+            <a href="movie-details.php?id=<?= $movie['movie_id'] ?>">Details</a>
+        <?php endforeach; ?>
 
 </body>
 
