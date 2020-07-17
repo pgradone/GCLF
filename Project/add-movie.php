fc8cc4c55d368984a5100a0dbfb6a61dd6a98a6f
unknown <pgradone@gmail.com>
2020-07-17 09:04:20 +0200

    add form to add-movie.php

--- Project/add-movie.php
+++ Project/add-movie.php
@@ -14,6 +14,15 @@
     <div class="sub-nav-title">
         <h5>Selected film : title of film</h5>
     </div>
+    <h2 style="text-align:center"> Add movie to the database</h2> <br>
+    <form class="insertmovie" action="" method="post" style="text-align: center">
+        <input type="text" name="title" placeholder="Movie title" value=""><br><br>
+        <input type="text" name="year_released" placeholder="year of release" value=""><br><br>
+        <input type="text" name="synopsis" placeholder="Synopsis" value=""><br><br>
+        <input type="text" name="poster" placeholder="Poster" value=""><br><br>
+        <input type="text" name="category_id" placeholder="Category" value=""><br><br>
+        <input type="submit" name="createmovie" value="Create movie">
+    </form>
 </body>
 
 </html>
